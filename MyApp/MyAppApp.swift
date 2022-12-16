f54e959a113f37312960efca9af6f375bed86f53
Nadia <ivashchenko.nadia@gmail.com>
2022-12-16 12:31:08 +0200

    Initial Commit

---
+++ MyApp/MyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAppApp.swift
+//  MyApp
+//
+//  Created by Students on 16.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct MyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
